72f6a63eca5f5c2b4a27aa6814e2cd061c322f53
Kaviselvan-SJ <sjkaviselvan07@gmail.com>
2025-04-04 22:46:13 +0530

    Initial commit

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
-const MONGO_URI = 'mongodb://localhost:27017/eventsDB'; // Your DB name
+const MONGO_URI ='mongodb+srv://sjkaviselvan07:jCjFucCyrN1odG3t@sdcdb.ydyz4gl.mongodb.net/?retryWrites=true&w=majority&appName=SDCDB';
+
+
 
 // Enable CORS & Body Parsing
 app.use(cors());
-app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('✅ MongoDB Atlas connected!'))
+    .catch(err => console.error('❌ MongoDB connection error:', err));
+
 
 // Define Event Schema & Model
 const eventSchema = new mongoose.Schema({
@@ -43,6 +45,10 @@ const Event = mongoose.model('Event', eventSchema);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
 // Increase request body size limit
 app.use(express.json({ limit: "50mb" })); // Allows up to 10MB JSON payload
 app.use(express.urlencoded({ limit: "50mb", extended: true })); // For form data
